8e18d5663dd3a19f6effd42acbcede26938ea0af
Christian Treppo <christian@treppo.org>
2021-02-11 12:06:40 +0100

    Nicer error pages

--- src/clj/chattercook/handler.clj
+++ src/clj/chattercook/handler.clj
@@ -25,11 +25,11 @@
         (wrap-webjars (constantly nil)))
       (ring/create-default-handler
         {:not-found
-         (constantly (error-page {:status 404, :title "404 - Page not found"}))
+         (constantly (error-page {:status 404, :title "Page not found"}))
          :method-not-allowed
-         (constantly (error-page {:status 405, :title "405 - Not allowed"}))
+         (constantly (error-page {:status 405, :title "Not allowed"}))
          :not-acceptable
-         (constantly (error-page {:status 406, :title "406 - Not acceptable"}))}))))
+         (constantly (error-page {:status 406, :title "Not acceptable"}))}))))
 
 (defn app []
   (middleware/wrap-base #'app-routes))
